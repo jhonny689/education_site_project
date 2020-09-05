9eabaee2c4b66f36f7f0e796a4802be951e32ba0
kain <kaindoneson@gmail.com>
2020-09-05 19:19:16 -0400

    working on fb omni with devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,6 +271,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
+  # example omni setup:
+  config.omniauth :facebook, '347280399762274', '5084d093c6e175622dffaaac0ffa3950', callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  #config.omniauth :facebook, "351938181836358", "eb46110fbc3b52113dfd7c54cba43737", callback_url: "http://localhost:3000/users/auth/facebook/callback"
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   # ==> Warden configuration
@@ -294,7 +297,7 @@ Devise.setup do |config|
   #
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
-  # config.omniauth_path_prefix = '/my_engine/users/auth'
+  #config.omniauth_path_prefix = '/my_engine/users/auth'
 
   # ==> Turbolinks configuration
   # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
